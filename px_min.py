f066d29c82980ee088dda29c76227d746c2be535
Benjamin David Walcott <benjamin.walcott@gmail.com>
2017-10-24 13:42:51 -0400

    Added option-parsing

--- px_min.py
+++ px_min.py
@@ -2,7 +2,22 @@ import optparse
 from rosetta import *
 
 def main():
-    None
+    parser = optparse.OptionParser()
+    # pdb input option
+    parser.add_option("-f","--file",dest="pdb",help="Input PDB file to be energy minimized",metavar="in.pdb")
+    # pdb output option
+    parser.add_option("-o","--out",dest="outFile",help="Filename of minimized PDB to be output",metavar="out.pdb")
+    # Alanine insertion location option
+    parser.add_option("-a","--Ala",type="int",dest="alaRegions",nargs=4,help="Beginning and end residue numbers for both polyA regions of T7 endoI",metavar="ABeg AEnd BBeg BEnd")
+    
+    (options,args) = parser.parse_args()
+    
+    PdbIn = options["pdb"]
+    PdbOut = options["outFile"]
+    AlaReg1 = options["alaRegions"][:2]
+    AlaReg2 = options["alaRegions"][2:]
+    
+    
     
 def addConstraints():
     None
