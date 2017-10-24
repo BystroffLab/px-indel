7bb7d53667e69852e98316a73b13dae08c05611b
Benjamin David Walcott <benjamin.walcott@gmail.com>
2017-10-24 12:37:41 -0400

    Added function definitions

---
+++ px_min.py
@@ -0,0 +1,18 @@
+import optparse
+from rosetta import *
+
+def main():
+    None
+    
+def addConstraints():
+    None
+    
+def genSymmetry():
+    ''' Generate the symmetry files for use in rosetta '''
+    None
+
+def runMinimization():
+    None
+
+
+if __name__ == "__main__": main()
