c9d59bd5c9b639b3a699536bfffdda8e39c5ada2
Benjamin David Walcott <benjamin.walcott@gmail.com>
2017-10-26 14:26:02 -0400

    Modified for continuous numbering.  Only 1 Ala region

--- px_min.py
+++ px_min.py
@@ -7,26 +7,25 @@ import subprocess
 # from rosetta import *
 
 def main():
+    '''PDB must be numbered continuously'''
     parser = optparse.OptionParser()
     # pdb input option
     parser.add_option("-f","--file",dest="pdb",help="Input PDB file to be energy minimized",metavar="in.pdb")
     # pdb output option
     parser.add_option("-o","--out",dest="outFile",help="Filename of minimized PDB to be output",metavar="out.pdb")
     # Alanine insertion location option
-    parser.add_option("-a","--Ala",type="int",dest="alaRegions",nargs=4,help="Beginning and end residue numbers for both polyA regions of T7 endoI",metavar="ABeg AEnd BBeg BEnd")
+    parser.add_option("-a","--Ala",type="int",dest="alaRegions",nargs=2,help="Beginning and end residue numbers for both polyA regions of T7 endoI",metavar="ABeg AEnd BBeg BEnd")
     parser.add_option("-c","--cst",dest="cst",help="cst file in which to save constraints",metavar="constraints.cst",default="constraints.cst")
     
     (options,args) = parser.parse_args()
     
     PdbIn = options["pdb"]
     PdbOut = options["outFile"]
-    AlaReg1 = options["alaRegions"][:2]
-    AlaReg2 = options["alaRegions"][2:]
+    AlaRegion = options["alaRegion"]
     
+
     
-    
-def addConstraints(structure,region1,region2):
-    PXChains = ["C","D","E","F"]
+def addConstraints(structure,PXChains=["C","D","E","F"]):
     constraints = []
     # Get PX CoordinateConstraints
     for chain in PXChains:
@@ -34,13 +33,12 @@ def addConstraints(structure,region1,region2):
             for (atom,(x,y,z)) in genCoordinates(structure,chain,res):
                 # CoordinateConstraint atom res atom2 res2 x y z HARMONIC x0 sd
                 constraint = "CoordinateConstraint %s %i%c N 1A %f %f %f HARMONIC 1.0 0.1"%(atom,res,chain,x,y,z)
-                constraints.append(constraint)
-    #generate beta-beta constraints between region1 and region2            
+                constraints.append(constraint)           
     return constraints
     
     
 def genSymmetry(pdb,m='NCS',a='A',i='B',d='C D E F'):
-    ''' Generate the symmetry files for use in rosetta.  calls the
+    '''Generate the symmetry files for use in rosetta.  calls the
     make_symmdef_file.pl script
     m is the symmetry type
     a is the first chain of the symmetric group
@@ -55,8 +53,13 @@ def genSymmetry(pdb,m='NCS',a='A',i='B',d='C D E F'):
     out = open("%s.symm"%(pdb.split(".pdb")[0]),"w+")
     out.write(stdoutdata)
     out.close()
+    # Return False if there is an error
     if p.returncode != 0: return False
     return True
+
+def makeBlueprint(pdb,region1,region2):
+    '''Generates the blueprint file for use with RosettaRemodel'''
+    None
 
 def runMinimization():
     None
